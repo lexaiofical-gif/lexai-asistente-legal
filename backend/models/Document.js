1ef2f33d891312bee6549f27ea52b803ed20df39
lexaiofical-gif <lexai.ofical@gmail.com>
2025-11-07 19:59:46 -0500

    Enhance Document.js with additional comments
    Added comments for clarity and improved documentation.

--- backend/models/Document.js
+++ backend/models/Document.js
@@ -1,6 +1,9 @@
+//DEFINE EL MOLDE DEL DOCUMENTO SELECCIONADO POR EL USUARIO  UNICO SE ENLAZA CON USER.JS PARA VER QUIEN ES EL DUEÑO
+      //IMPORTA A MONGO PARA CREAR LA ESTRUCTURA DE DE DATOS DE LOS DOCUME
 const mongoose = require('mongoose');
 
 const documentSchema = new mongoose.Schema({
+    //ENLAZA EL DOCUMENTO CON EL USUARIO Q LO CREO
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
@@ -11,6 +14,7 @@ const documentSchema = new mongoose.Schema({
         required: true,
         enum: ['Contrato de Prestación de Servicios', 'Certificado Laboral', 'Carta de Renuncia']
     },
+    //ALMACENA EL TEXTO COMPLETO DE LOS DOCUMENTS
     content: {
         type: String,
         required: true
@@ -25,5 +29,5 @@ const documentSchema = new mongoose.Schema({
 
 // Índice para búsquedas por usuario
 documentSchema.index({ userId: 1, timestamp: -1 });
-
+//CREA Y EXPORTA EL MODELO PARA INTERACTUAR CON LA COLLECCION DE DOCUMENTOS
 module.exports = mongoose.model('Document', documentSchema);
